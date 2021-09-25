6b9ce5bc116ee5cd2071ba99ad2264a1122dc9b7
ChrisAntem <79417622+ChrisAntem@users.noreply.github.com>
2021-09-25 22:40:13 +0200

    Update tagline.js

--- assets/js/tagline.js
+++ assets/js/tagline.js
@@ -418,6 +418,8 @@ const taglines = [
     "https://danstonchat.com/19936.html",
     "Il faut rendre à La Délégation ce qui est à César.",
     "La Tassitude, c'est tout ce dont vous n'avez pas besoin"
+    "Un projet en bêta tenu par des bêtas"
+    "Des slogans de boxe."
 ];
 
 const generateRandomInteger = (min = 0, max = 1) => {
